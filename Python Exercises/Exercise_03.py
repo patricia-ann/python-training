f2fa75937d580d9ce3eb156a622064177947b718
patriciaann.parrocha <patriciaann.parrocha@asurion.com>
2021-05-12 18:15:35 +0800

    update solution

--- Python Exercises/Exercise_03.py
+++ Python Exercises/Exercise_03.py
@@ -4,5 +4,4 @@ name = input("Enter your name:")
 age = input("Enter your age:")
 
 print(f"Hello my name is {name}, Age is {age}")
-print("Hello my name is", name, "Age is, ", age)
-print("Hello my name is " + name + ' ' + "Age is," + age)
+
